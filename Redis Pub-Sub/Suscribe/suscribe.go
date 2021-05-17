152d76c84fbd5df284286e090b2bf63eeb539615
Cristian Azurdia <1983964490101@ingenieria.usac.edu.gt>
2021-05-16 23:35:12 -0600

    Correciones Redis Pub Sub
    Conexión a Mongo DB
    Conexión a REdis por medio de go-redis (metodo Do)

--- Redis Pub-Sub/Suscribe/suscribe.go
+++ Redis Pub-Sub/Suscribe/suscribe.go
@@ -5,10 +5,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"strconv"
 	"time"
 
 	"github.com/go-redis/redis/v8"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -48,25 +50,20 @@ func main() {
 	// Itterate any messages sent on the channel
 	for msg := range channel {
 		u := &msg_COVID{}
+
 		// Unmarshal the data into the user
 		err := u.UnmarshalBinary([]byte(msg.Payload))
 		if err != nil {
 			panic(err)
 		}
 
-		fmt.Println(u)
-
 		//conexión con MOngo DB
-		insert_mongo(u.String())
-		// postBody := []byte(u.String())
-		// fmt.Println("Response: enviando a Mongo DB")
-		// http.Post("http://35.229.87.37:27017", "application/json", bytes.NewBuffer(postBody))
-		// fmt.Println("Response : datos fueron enviados")
+		insert_mongo(u)
 
 	}
 }
 
-func insert_mongo(jsonString string) {
+func insert_mongo(msgcovid *msg_COVID) {
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://35.229.87.37:27017").SetAuth(options.Credential{
 		AuthSource: "testdb", Username: "jossie", Password: "grupoalgo",
 	}))
@@ -74,29 +71,56 @@ func insert_mongo(jsonString string) {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 
 	err = client.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	defer client.Disconnect(ctx)
 
 	//Accessing the collection inside the database
 	collection := client.Database("testdb").Collection("covid")
 
-	//Parsing JSON string to Struct
-	var req msg_COVID
-	json.Unmarshal([]byte(jsonString), &req)
-
-	//Checking valid struct
-	if (msg_COVID{} != req) {
-		//Inserting into Mongodb
-		insertResult, err := collection.InsertOne(context.TODO(), req)
+	//Inserting into Mongodb
+	insertResult, err := collection.InsertOne(context.TODO(), msgcovid)
+	fmt.Println("SE INSERTO EN LA BASE DE DATOS")
+	fmt.Println(insertResult)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted post with ID:", insertResult.InsertedID)
+	// Create a new Redis Client
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "35.196.165.91:6379", // We connect to host redis, thats what the hostname of the redis service is set to in the docker-compose
+		Password: "",                   // The password IF set in the redis Config file
+	})
+	// Ping the Redis server and check if any errors occured
+	err2 := redisClient.Ping(context.Background()).Err()
+	if err2 != nil {
+		// Sleep for 3 seconds and wait for Redis to initialize
+		time.Sleep(3 * time.Second)
+		err2 := redisClient.Ping(context.Background()).Err()
 		if err != nil {
-			log.Fatal(err)
+			panic(err2)
 		}
-		fmt.Println("Inserted post with ID:", insertResult.InsertedID)
+	}
+	//Inserting into redis
+	// Loop and randomly generate users on a random timer
+	for {
+		// Publish a generated user to the new_users channel
+		res, err2 := redisClient.Do(ctx, "LPUSH", "lista", msgcovid.String()).Result()
+
+		if err != nil {
+			panic(err2)
+		}
+		fmt.Println(res)
+		// Sleep random time
+		rand.Seed(time.Now().UnixNano())
+		n := rand.Intn(4)
+		time.Sleep(time.Duration(n) * time.Second)
 	}
 }
 
@@ -108,7 +132,7 @@ func (u *msg_COVID) MarshalBinary() ([]byte, error) {
 
 // UnmarshalBinary decodes the struct into a msgCovid
 func (u *msg_COVID) UnmarshalBinary(data []byte) error {
-	if err := json.Unmarshal(data, u); err != nil {
+	if err := json.Unmarshal(data, &u); err != nil {
 		return err
 	}
 	return nil
